1c46caee302818c27be56c35c1b616a5a5fc5d33
Jaibw <jai.unix@gmail.com>
2025-02-09 22:35:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+import csv  ## import csv pkg
+ 
+with open('data.csv', mode ='r')as file:   ## open data.csv and refer it as a file
+  csvFile = csv.reader(file,delimiter=';') ## read csv content
+  next(csvFile) ## skip the header 
+  for lines in csvFile:  ## loop every line 
+        print(lines) ## print the each row 
